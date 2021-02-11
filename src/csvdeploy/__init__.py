802c3377543b11c0be13b58a3f122435d29e1ce3
Tiago Tresoldi <tresoldi@shh.mpg.de>
2021-02-11 12:18:08 +0100

    Add dummy Python files

---
+++ src/csvdeploy/__init__.py
@@ -0,0 +1,13 @@
+# __init__.py
+
+"""
+__init__.py file for the `csvdeploy` library.
+"""
+
+# Package information for the library
+__version__ = "0.1"
+__author__ = "Tiago Tresoldi"
+__email__ = "tiago.tresoldi@lingfil.uu.se"
+
+# Build the namespace
+from .common import dummy_func
---
+++ src/csvdeploy/__main__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+__main__.py
+
+Module for command-line execution of alignment.
+"""
+
+# Import our library
+import csvdeploy
+
+def main():
+    """
+    Entry point.
+    """
+
+    print(csvdeploy.dummy_func())
+
+if __name__ == "__main__":
+    main()
